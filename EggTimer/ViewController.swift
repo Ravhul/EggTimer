20831089eaf5de165fb89f7b0545abd3e949d3fc
Ravhul <rahulsingh7965@gmail.com>
2022-06-04 18:36:13 +0530

    Project Updated

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,24 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var softTime = 8;
+    var softTime = 1;
     var medium = 12
     var hard = 16;
-
+    
+    var minRmn = 1;
+    var sec = 60;
+    var ttlTme = 1;
+    
+    @IBOutlet weak var titleText: UILabel!
+    @IBOutlet weak var prgressBar: UIProgressView!
+    var btnType:String? = nil;
+    var timer = Timer();
+    
     @IBAction func eggBtnClk(_ sender: UIButton) {
-        let btnType = sender.currentTitle;
-        var eggTime : Int;
+        btnType = sender.currentTitle;
+        timer.invalidate();
         
         if btnType! == "Soft" {
-            eggTime = softTime;
+            minRmn = softTime;
         }else if(btnType! == "Medium"){
-            eggTime = medium;
+            minRmn = medium;
         }
         else{
-            eggTime = hard;
+            minRmn = hard;
         }
+        ttlTme = minRmn*sec;
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil,repeats: true);
+    }
+    
+    @objc func updateTimer(){
+        if minRmn > 0{
+            let perProg : Float = Float(61 - sec) / Float(ttlTme);
+            print("\(perProg)");
+            prgressBar.progress = perProg;
+            titleText.text = "\(btnType!) Egg will cook in \(minRmn-1) : \(sec)";
+        }else{
+            titleText.text = "\(btnType!) Egg Cooked!";
+            timer.invalidate();
+        }
+        sec-=1;
         
-        print("\(btnType!) will cook in \(eggTime) min.");
+        if sec <= 0{
+            minRmn-=1;
+            sec = 60;
+        }
     }
     
    
